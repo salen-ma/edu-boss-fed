ec69a28617061d265f34debc6d674b27673c43f3
mashaoliang <salen.ma@qq.com>
2020-10-15 23:39:50 +0800

    feat: 添加基本布局

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,25 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Layout from '@/layout/index.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    component: Layout,
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: () => import(/* webpackChunkName: 'home' */ '@/views/home/index.vue')
+      }
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '*',
+    name: '404',
+    component: () => import(/* webpackChunkName: '404' */ '@/views/error-page/404.vue')
   }
 ]
 
---
+++ src/views/home/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    home
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-property-decorator'
+export default class Home extends Vue {}
+</script>
