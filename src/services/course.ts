21ee3c4deb495ede359f9810510237759dafdf46
mashaoliang <salen.ma@qq.com>
2020-11-11 23:17:50 +0800

    feat: 更新课程列表

---
+++ src/services/course.ts
@@ -0,0 +1,21 @@
+/**
+ * 课程相关请求模块
+ */
+
+import request from '@/utils/request'
+
+export const getQueryCourses = (data: any) => {
+  return request({
+    method: 'POST',
+    url: '/boss/course/getQueryCourses',
+    data
+  })
+}
+
+export const changeState = (params: any) => {
+  return request({
+    method: 'GET',
+    url: '/boss/course/changeState',
+    params
+  })
+}
